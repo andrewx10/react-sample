e5f3e96c34ffa54e0db448932645782b51203291
Andrew Shaw <ashaw85@hotmail.com>
2019-09-23 08:22:56 -0700

    pass userAndId down

--- src/billing.js
+++ src/billing.js
@@ -22,3 +22,23 @@ class BillingWidget extends React.Component {
 
 const billingDomContainer = document.querySelector('#bill_button_container');
 ReactDOM.render(React.createElement(BillingWidget), billingDomContainer);
+
+function UserAndId(props) {
+  const user = props.user;
+  console.log('header');
+  return <div id="hello">Hello {user}, id {props.userId}</div>;
+}
+
+function Intro(props) {
+  const userAndId = (
+      <UserAndId user={props.user} userId={props.userId}/>
+      );
+  return <UserInfo userAndId={userAndId} />
+}
+
+function UserInfo(props) {
+  console.log('UserInfo');
+  return <div id="userInfo">{props.userAndId}</div>
+}
+
+ReactDOM.render(<Intro user="Joan" userId="134"/>, document.getElementById('intro'));
